ef93d9cbf69dba5ec2a1b0426d492ebeeedf1a39
matic31 <maticnk@gmail.com>
2015-04-19 00:52:20 +0200

    view za testiranje modela

--- application/views/test_view.php
+++ application/views/test_view.php
@@ -1,6 +1,6 @@
 <?php
 //print_r($proizvod);
-echo $proizvod[0]['Ptitle']."<br>";
+/*echo $proizvod[0]['Ptitle']."<br>";
 echo $proizvod[0]['modelOpis']."<br>";
 echo $proizvod[0]['osobine']."<br>";
 $osobineN=  explode('*', $proizvod[0]['osobine']);
@@ -8,7 +8,7 @@ $broj=count($osobineN);
     for($i=0; $i<$broj-1; $i++){
        $osobina=  explode('|', $osobineN[$i]);
         echo $osobina[0].": ".$osobina[1]." ".$osobina[2]."<br>";
-}
+}*/
 
 ?>
 
