26085a15c24d4eeedaf821d2143ae42271a5572a
matic31 <maticnk@gmail.com>
2015-04-20 19:22:19 +0200

    fajl sa izmenama za bazu

---
+++ fajlovi/izmeneBaze.sql
@@ -0,0 +1,8 @@
+--Sledece izmene baze važe za poslednju verziju exportovane baze 19.04.2015
+--Naziv Commit-a: NOVI SQL!!! (19.04)
+--Putanja: https://github.com/ICTCrew/shop/commit/01b2249750d654982bf348538c61b913d6abc326#diff-ad7451e00b293386405e78c1632ec0e6
+
+
+
+--Dodavanje statusa u proizvod_grupa (1)
+ALTER TABLE `proizvod_grupa` ADD `status` TINYINT(1) NOT NULL AFTER `status`;
