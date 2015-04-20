3bce4d740f0f7b8774b63ea02a4f70d9e0d1758c
matic31 <maticnk@gmail.com>
2015-04-20 15:09:38 +0200

    skinuo se sa FC-a

--- application/controllers/test_controller.php
+++ application/controllers/test_controller.php
@@ -5,7 +5,7 @@
  *
  * @author Matic
  */
-class Test_Controller extends FrontEnd_Controller{
+class Test_Controller extends CI_Controller{
     
     public function __construct() {
         parent::__construct();
@@ -21,18 +21,18 @@ class Test_Controller extends FrontEnd_Controller{
     
     public function index() {
        
-        $data=  $this->admin_model->get('proizvod');
+        $data['sve']=  $this->admin_model->get('proizvod');
         
         
-        $data=  $this->proizvod_model->getProizvodiBrenda(1);
+        $data['sve']=  $this->proizvod_model->getProizvodiBrenda(1);
         
         
         $data1=array('idProizvod', 'modelOpis', 'opis');
-        $data=  $this->proizvod_model->getKolone('proizvod', $data1, 2,1);
+        $data['sve']=  $this->proizvod_model->getKolone('proizvod', $data1, 2,1);
         
         
         
-        $data=  $this->frontend_model->getKategorija();
+        $data['sve']=  $this->frontend_model->getKategorija();
         
         $niz=array('prvi', 'drugi', 'treci', 'cet', 'peti', 'sesti', 'sedmi', 'osmi', 'deveti', 'deseti');
         
@@ -42,49 +42,50 @@ class Test_Controller extends FrontEnd_Controller{
         
         
         
-        $data=  $this->korisnik_model->getKorisnik('bilja@bla.com', 'bilja86');
+        $data['sve']=  $this->korisnik_model->getKorisnik('bilja@bla.com', 'bilja86');
         
-        $data=  $this->korisnik_model->getPorudzbine(1);
+        $data['sve']=  $this->korisnik_model->getPorudzbine(1);
         
         $data['osobine']=  $this->admin_model->prepisiOsobine(1);
         
         $data['proizvod']=  $this->proizvod_model->getProizvod(1);
         
-        $data=  $this->frontend_model->getGrupa();
+        $data['sve']=  $this->frontend_model->getGrupa();
         
-        $data=  $this->frontend_model->getBrend();
+        $data['sve']=  $this->frontend_model->getBrend();
         
-        $data=  $this->frontend_model->getStranica('url stranica');
+        $data['sve']=  $this->frontend_model->getStranica('url stranica');
         
-        $data=  $this->frontend_model->conf_shop();
+        $data['sve']=  $this->frontend_model->conf_shop();
         
-        $data=  $this->frontend_model->getSlajder();
+        $data['sve']=  $this->frontend_model->getSlajder();
         
-        $data=  $this->frontend_model->getMeni();
+        $data['sve']=  $this->frontend_model->getMeni();
         
-        $data=  $this->backend_model->getOpcije(1);
+        $data['sve']=  $this->backend_model->getOpcije(1);
         
-        $data=  $this->proizvod_model->search("opis2");
+        $data['sve']=  $this->proizvod_model->search("opis2");
         
-        $data=  $this->login_model->getKorisnik('bilja@bla.com', 'bilja86');
+        $data['sve']=  $this->login_model->getKorisnik('bilja@bla.com', 'bilja86');
         
-        $data=  $this->login_model->activateUser('123123123123');
+        $data['sve']=  $this->login_model->activateUser('123123123123');
         
-        $data=  $this->proizvod_model->getProizvodiGrupe(1);
+        $data['sve']=  $this->proizvod_model->getProizvodiGrupe(1);
         
-        $data=  $this->proizvod_model->getPovezaniProizvodi(1);
+        $data['sve']=  $this->proizvod_model->getPovezaniProizvodi(1);
         
-        $data=  $this->proizvod_model->getProizvodiKategorije(1, 'modelOpis', 'desc');
+        $data['sve']=  $this->proizvod_model->getProizvodiKategorije(1, 'modelOpis', 'desc');
         
-        $data=  $this->proizvod_model->getKomentari(1);
+        $data['sve']=  $this->proizvod_model->getKomentari(1);
         
-        $data=  $this->catalogue_model->getKategorije();
+        $data['sve']=  $this->catalogue_model->getKategorije();
         
-        $data=  $this->catalogue_model->getBrend();
+        $data['sve']=  $this->catalogue_model->getBrend();
+        
         //$data['title']='Home';
         //$data['ulogovan']=false;
         
-        $this->load_view_test($data);
+        $this->load->view('test_view',$data);
     }
     
     
--- application/core/frontend_controller.php
+++ application/core/frontend_controller.php
@@ -13,20 +13,6 @@ class frontend_controller extends MY_Controller {
         $this->load->model('frontend_model');
     }
     
-    public function load_view_test($data) {
-        /*
-        $this->load->model('frontend_model');
-        $this->load->view('template/header', $data);
-        $this->load->view('template/aside', $data);
-        $this->load->view('template/footer', $data); 
-         */
-        echo "<a href='http://localhost/shop/'>http://localhost/shop</a>";
-        echo '<br>';
-        echo "<a href='http://localhost/shop/index.php/test_controller'>http://localhost/shop/index.php/test_controller</a>";
-        echo '<br><br><br><br><br>';
-        $data1['sve']=$data;
-        $this->load->view('test_view', $data1);
-    }
     
     public function loadView() {
         
