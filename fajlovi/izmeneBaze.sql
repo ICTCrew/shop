6dac44696ea07de357070ad8a812eee8672aeb1d
matic31 <maticnk@gmail.com>
2015-04-20 19:26:17 +0200

    fajl sa izmenama za bazu

--- fajlovi/izmeneBaze.sql
+++ fajlovi/izmeneBaze.sql
@@ -1,7 +1,8 @@
 --Sledece izmene baze važe za poslednju verziju exportovane baze 19.04.2015
 --Naziv Commit-a: NOVI SQL!!! (19.04)
 --Putanja: https://github.com/ICTCrew/shop/commit/01b2249750d654982bf348538c61b913d6abc326#diff-ad7451e00b293386405e78c1632ec0e6
-
+--Posle opisa u komenataru navesti i redni broj zbog praćenja redosleda
+--Primer i prva izmena ispod:
 
 
 --Dodavanje statusa u proizvod_grupa (1)
