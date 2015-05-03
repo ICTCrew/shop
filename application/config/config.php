ab8e808c71f1ce8c5e4d92a5ad128127aab440c5
kacca <kacca93th@gmail.com>
2015-05-03 17:42:09 +0200

    vratila sam rute i conf

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url']	='http://localhost/shop/shop';
+$config['base_url']	='http://localhost/shop';
 
 function __autoload($class)
 {
