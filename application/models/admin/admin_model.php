4c697f98cd66dcac8fd852bc2c7744cb3b8df0f0
matic31 <maticnk@gmail.com>
2015-06-24 14:09:05 +0200

    izmene admin model

--- application/models/admin/admin_model.php
+++ application/models/admin/admin_model.php
@@ -23,5 +23,18 @@ class admin_model extends CI_Model{
         return $query->result_array();
     }
     
+    public function getUloge() {
+        $query = $this->db->select('idUloga, nazivUloga, status');
+        $query=  $this->db->get('uloga');
+        return $query->result_array();
+    }
+    
+    public function getUloga($id) {
+        $query = $this->db->select('idUloga, nazivUloga, status');
+        $query=  $this->db->where('idUloga', $id);
+        $query=  $this->db->get('uloga');
+        return $query->result_array();
+    }
+    
     
 }
